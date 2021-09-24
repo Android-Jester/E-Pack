72f565647cd00fa6dd8c6548baa2258a74c943b3
AndroidJester <duah14@outlook.com>
2021-09-24 21:18:30 +0000

    hello darkness my old friend

--- lib/features/requestBuilder/presentation/pages/page_1.dart
+++ lib/features/requestBuilder/presentation/pages/page_1.dart
@@ -1,14 +1,51 @@
+import 'package:e_pack/features/requestBuilder/presentation/config/config.dart';
 import 'package:flutter/material.dart';
 
 class Page1 extends StatelessWidget {
   static String id = "Page1";
   const Page1({Key? key}) : super(key: key);
-
+  Config config = new Config();
   @override
   Widget build(BuildContext context) {
+    config.init(context);
     return Scaffold(
       body: Column(
-        children: const [],
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(config.itemWidth(20.0)),
+            ),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back_ios),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: MaterialStateColor.resolveWith((states) => Colors.white),
+            ),
+          ),
+          TextButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateColor.resolveWith((states) => Colors.grey),
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(config.itemWidth(20.0)),
+                ),
+              ),
+            ),
+            child: const Text("Book Now"),
+            onPressed: () => Navigator.pushNamed(context, Page2.id),
+          ),
+        ],
       ),
     );
   }
